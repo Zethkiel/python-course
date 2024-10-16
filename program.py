f2815cf95971aae4c87c45846fa55eef822b84d3
Mar Joseph M. Bijer <70209257+Zethkiel@users.noreply.github.com>
2024-10-16 09:18:40 +0000

    bleh

--- program.py
+++ program.py
@@ -1,1 +1,11 @@
-print(round(1.5))
+def factorial(num):
+   call_stack = []
+   if num == 1:
+       print('base case reached! Num is 1.')
+       return 1
+   else:
+       call_stack.append({'input': num})
+       print('call stack: ', call_stack)
+       return num * factorial(num-1)
+ 
+factorial(3)
